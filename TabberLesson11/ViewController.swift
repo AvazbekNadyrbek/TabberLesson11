e430099a2ece718810b49330762a42bd9b928b84
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-06-06 15:56:36 +0200

    Initial Commit

---
+++ TabberLesson11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabberLesson11
+//
+//  Created by Авазбек Надырбек уулу on 06.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
